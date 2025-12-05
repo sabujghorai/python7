51064e044e4e20e0aa4a8ce1787d5ae436a61096
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-05 19:53:15 +0530

    Implement tuple index and count examples
    Add tuple operations to demonstrate index and count methods

---
+++ tup_method.py
@@ -0,0 +1,3 @@
+hello = (1,3,4,6,5,7,7,6,5,5,5,6,7,5,3)
+print(hello.index(5)) #here 5 is the element it's not a index
+print(hello.count(6),hello.count(5)) #counts the total number of occurence of element

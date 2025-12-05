daa6cbbea6ca8f859cb0cac2a5ff4493c6df059e
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-05 19:52:39 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,6 @@
+tuple = () # we can create a empty tuple
+tuple = (1) # it is a wrong way to assign a tuple
+tuple = (1,) # a comma is compalsury to create a tuple and this is the right methood to create a tuple
+print(type(tuple))
+tup = (3,4,5,6,2,6,2)
+print(type(tup))
